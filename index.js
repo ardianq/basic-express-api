85f87c1bb42d4cda36e76b530908a8b79e131603
ardianq <ardian.qerimi3@gmail.com>
2021-11-03 11:25:12 +0100

    Ardian testet

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || '3000';
+
+app.get('/', (req, res) => {
+  res.send('Your Express API is up and running!');
+});
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`);
+});
