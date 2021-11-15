f28fd8d9d2c5f88b5341e0e6c3d21d3c82fcedbb
Ardian <33004337+ardianq@users.noreply.github.com>
2021-11-15 11:24:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || '3000';
 
 app.get('/', (req, res) => {
-  res.send('Your Express API is up and running Ardian # test 3!');
+  res.send('Your Express API is up and running Ardian');
 });
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`);
